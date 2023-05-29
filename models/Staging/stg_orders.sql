d07309afcbac87d27b79d41c14e7196f0a96419e
Deepthi Gopavaram <gopavaramdeepthi@gmail.com>
2023-05-29 06:35:59 +0000

    added macros

--- models/Staging/stg_orders.sql
+++ models/Staging/stg_orders.sql
@@ -13,7 +13,8 @@ c.country,
 p.category,
 p.productid,
 p.productname,
-p.subcategory
+p.subcategory,
+{{ markup() }} as markup
 from {{ ref('raw_orders') }} as o
 left join {{ref("raw_customres")}} as c
 on o.customerid=c.customerid
